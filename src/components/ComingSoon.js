654dcd79b11d8162a45deba95a05163d7599ecfb
[vallin1991] <[victorvallinavila@gmail.com]>
2022-05-16 11:31:22 -0700

    Creating a single a page for any webpage of coming soon

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,12 @@
+import '../styles/ComingSoon.css';
+
+function ComingSoon() {
+    return (
+        <div className="background">
+            <h1>Coming Soon</h1>
+
+        </div>
+    );
+}
+
+export default ComingSoon;
